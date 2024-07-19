2da5835f69e7f73631a3e2d766b87c22f6128aa6
Alex Melo <alex_100894@gmail.com>
2024-07-19 16:54:59 -0300

    build: First instance of the project created.

--- testes.py
+++ testes.py
@@ -1,10 +1,6 @@
 from components.Files_Handler.module.file_handler import Files_Handling
 import sqlite3
-
-
-
-
-
+from datetime import datetime
 
 class inventory:
     def __init__(self, base_name) -> None:
@@ -12,24 +8,66 @@ class inventory:
         self.base_name = base_name
         self.file_handling = Files_Handling()
 
-    def invenory_create(self):
+    def inventory_create(self):
+        data = self.file_handling.read_file('inver.json', pattern_folder= 'D:/@work/inventory/data/')
+        for key in data:
+            if 'data_de_entrada' == key:
+                while True:
+                    choose_date = input(f"""
+                    A data de entrada foi gerada, ({datetime.strftime(datetime.now(), "%d/%m/%Y")}) confirma essa data como data de entrada?
+
+                    APERTE 'ENTER' SE SIM | ou digite 'N' e aperte ENTER se NÃO """)
+                    if choose_date == '':
+                        data[key].append(datetime.strftime(datetime.now(), "%y%m%d"))
+                        break
+                    elif choose_date.lower() == 'n':
+                        day_in = input("\nDigite o dia desejado: ")
+                        month_in = input("\nDigite o mês desejado: ")
+                        year_in = input("\nDigite o ano desejado: ")
+                        data[key]  = datetime(int(year_in), int(month_in), int(day_in)).strftime("%y%m%d")
+                        break
+            if 'id' == key:
+                data[key].append(int(input('Digite o id do produto que deseja cadastrar. (Apenas numeros!)  \n')))
+            if 'descrição' == key:
+                data[key].append(input('Descrição do produto. \n'))
+            if 'ponto_focal' == key:
+                data[key] = {}
+            if 'status' == key:
+                data[key] = ('disponivel', 'indisponivel')[0]
+            if 'serial_number' == key:
+                data[key].append(input('Digite o Id do produto que deseja cadastrar. \n'))
         
-        idd = input('Digite o Id do produto que deseja cadastrar')
-        data = self.items[idd] = '2'
+        self.file_handling.write_file(data, 'inver.json', pattern_folder= 'D:/@work/inventory/data/')
+        
+            
+            # key.append(input('teste'))
        
+        # 
 
-        self.file_handling.write_file(data, 'inver.json', pattern_folder= 'C:/Users/Alex_/Work/Área de Trabalho/Repo/inventory/')
-        pass
+        # self.items
+        # self.items['descrição'] = input('Descrição do produto. ')
+        # self.items
+        # self.items['status'] = 'disponivel' #tuple('disponivel', 'indisponivel')
+        # self.items['serial_number'] = input('Digite o Id do produto que deseja cadastrar')
+        # self.file_handling.write_file(self.items, 'inver.json', pattern_folder= 'D:/@work/inventory/data/')
+        # self.file_handling.append_file(data, 'inver.json', pattern_folder= 'C:/Users/Alex_/Work/Área de Trabalho/Repo/inventory/')
+        # self.items.update('id', )
 
-    def inventory_read(self):
-        pass
+       
+        
+        # idd = input('Digite o Id do produto que deseja cadastrar')
+        # data = self.items[idd] = '2'
+        # pass
+    
+    # def inventory_read(self):
+    #     pass
 
-    def inventory_update(self):
-        pass
+    # def inventory_update(self):
+    #     pass
 
-    def inventory_delete(self):
-        pass
+    # def inventory_delete(self):
+    #     pass
 
 
 
-print(inventory('').invenory_create())
+inventory('').inventory_create()
