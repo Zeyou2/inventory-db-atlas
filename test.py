43cce6b200255186952869808b9d0fda13b2c372
gabrielsbf <gabrielsbfernandes@gmail.com>
2024-11-12 20:56:04 -0300

    feat: made a new reset inv and replace items with 'teste' > later do with a better sample

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
 from src.inventory_handler import Handle_Operations
 
 op = Handle_Operations("central.json")
-print(op.put_required_elem("produtos", "Nome do produto"))
+# # print(op.filter_data_struct("usuarios", {'table_visible': 1}))
+# hidden = op.filter_data_struct("usuarios", {"form_editable": 'False'})
+# hidden = op.field_treatment(hidden)
+# print(hidden.values())
+op.reset_inv("categoria")
+op.reset_inv("pontos")
+op.reset_inv("produtos")
+op.reset_inv("transferência")
+op.reset_inv("usuarios")
