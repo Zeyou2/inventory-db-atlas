4c2c2b58f696559ad2fb60864980f33a7d4cbf8d
Zeyou2 <alex_100894@hotmail.com>
2024-12-02 17:49:43 -0300

    fix: :bug: fixed errors in edit forms

--- test.py
+++ test.py
@@ -2,13 +2,13 @@ from src.inventory_handler import Handle_Operations
 from src.connect import Mongo_Manager
 
 op = Handle_Operations("central.json")
-# # print(op.filter_data_struct("usuarios", {'table_visible': 1}))
+# # # print(op.filter_data_struct("usuarios", {'table_visible': 1}))
 # hidden = op.filter_data_struct("usuarios", {"form_editable": 'False'})
 # hidden = op.field_treatment(hidden)
-# print(hidden.values())
+# # print(hidden.values())
 # op.render_op_form("transferencia")
 # op.reset_inv("categoria")
-# op.reset_inv("pontos")
-# op.reset_inv("produtos")
+op.reset_inv("pontos")
+op.reset_inv("produtos")
 op.reset_inv("transferência")
-# op.reset_inv("usuarios")
+op.reset_inv("usuarios")
