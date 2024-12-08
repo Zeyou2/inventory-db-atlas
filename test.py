e106828091c232502d932ffd21ba86936de8d1e6
gabrielsbf <gabrielsbfernandes@gmail.com>
2024-12-07 22:06:22 -0300

    refactor: fixing some details

--- test.py
+++ test.py
@@ -2,17 +2,16 @@ from src.inventory_handler import Handle_Operations
 from src.connect import Mongo_Manager
 
 op = Handle_Operations("central.json")
-# # # print(op.filter_data_struct("usuarios", {'table_visible': 1}))
+# # print(op.filter_data_struct("usuarios", {'table_visible': 1}))
 # hidden = op.filter_data_struct("usuarios", {"form_editable": 'False'})
 # hidden = op.field_treatment(hidden)
 # print(hidden.values())
 
 # # op.render_op_form("transferencia")
-# op.reset_inv("categoria")
-# op.reset_inv("categorias")
-# op.reset_inv("pontos")
-# op.reset_inv("produtos")
-# op.reset_inv("transferência")
-# op.reset_inv("usuarios")
+op.reset_inv("categoria")
+op.reset_inv("pontos")
+op.reset_inv("produtos")
+op.reset_inv("transferencia")
+op.reset_inv("usuarios")
 
-print(op.dt_struct["produtos"].keys())
+# print(op.dt_struct["produtos"].keys())
